ebc5ae46e6acaf0e74a9991904313295fc92336d
Alex Dunn <adunn@ucsb.edu>
2016-03-29 14:34:18 -0700

    [deploy] don't change variables for no reason

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,5 +5,5 @@ set :default_env,
     'http_proxy' => '',
     'https_proxy' => ''
 
-server ENV['HOSTNAME'], user: ENV.fetch('USER', 'adrl'), roles: [:web, :app, :db, :resque_pool]
+server ENV['SERVER'], user: ENV.fetch('USER', 'adrl'), roles: [:web, :app, :db, :resque_pool]
 set :ssh_options, port: 22, keys: [ENV.fetch('KEYFILE', '~/.ssh/id_rsa')]
