10cb8fedff4095367bba63397ac8bdaad5af9633
Alex Dunn <adunn@ucsb.edu>
2016-03-29 12:10:46 -0700

    [deploy] use proxy for production deploys

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,9 @@
 set :stage, :production
 set :rails_env, 'production'
 
+set :default_env,
+    'http_proxy' => '',
+    'https_proxy' => ''
+
 server ENV['SERVER'], user: ENV.fetch('USER', 'adrl'), roles: [:web, :app, :db, :resque_pool]
 set :ssh_options, port: 22, keys: [ENV.fetch('KEYFILE', '~/.ssh/id_rsa')]
