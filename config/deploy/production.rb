4a88657cb5eafb304c1a2b874ddefd095a379723
Alex Dunn <adunn@ucsb.edu>
2016-05-04 16:08:59 -0700

    [deploy] default to NOT using proxy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,10 +1,10 @@
 set :stage, :production
 set :rails_env, 'production'
 
-# Comment out if deploying to a server with a public IP
-set :default_env,
-    'http_proxy' => '',
-    'https_proxy' => ''
+# Uncomment if deploying to a server without a public IP
+# set :default_env,
+#     'http_proxy' => '',
+#     'https_proxy' => ''
 
 server ENV['SERVER'], user: 'adrl', roles: [:web, :app, :db, :resque_pool]
 set :ssh_options, port: 22, keys: [ENV.fetch('KEYFILE', '~/.ssh/id_rsa')]
