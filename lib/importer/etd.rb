e1b4a2c073bcd7e68eca3bcca887bae7feee47ce
Alex Dunn <adunn@ucsb.edu>
2016-05-26 14:06:14 -0700

    Importer::ETD: document params

--- lib/importer/etd.rb
+++ lib/importer/etd.rb
@@ -2,7 +2,10 @@
 require 'proquest'
 
 module Importer::ETD
-  # @param [Array<String>] xml Paths to the MARC XML files
+  # Uses metadata from ProQuest XML files to query Aleph and build a
+  # MARCXML string
+  #
+  # @param [Array<String>] xml Paths to the ProQuest XML files
   # @return [String]
   def self.fetch_marc(xml)
     marcs = xml.map { |x| parse_file(x) }
@@ -15,10 +18,14 @@ module Importer::ETD
     EOS
   end
 
+  # @param [String] xml_file_name The path to a ProQuest XML file
+  # @return [String]
   def self.extract_binary_filename(xml_file_name)
     Nokogiri::XML(File.open(xml_file_name)).css('DISS_binary').children.first.to_s
   end
 
+  # @param [String] binary_filename The basename of the PDF for an ETD from ProQuest
+  # @return [String]
   def self.query_pegasus(binary_filename)
     search = Curl::Easy.perform("#{Rails.application.secrets.sru_host}/sba01pub?version=1.1&operation=searchRetrieve&maximumRecords=1&startRecord=1&query=(marc.947.a=pqd%20and%20marc.956.f=#{binary_filename})")
     result = search.body_str
@@ -29,10 +36,14 @@ module Importer::ETD
   PAYLOAD_HEADER = "<?xml version=\"1.0\"?>\n<zs:searchRetrieveResponse xmlns:zs=\"http://www.loc.gov/zing/srw/\"><zs:version>1.1</zs:version><zs:numberOfRecords>1</zs:numberOfRecords><zs:records>".freeze
   PAYLOAD_FOOTER = '</zs:records></zs:searchRetrieveResponse>'.freeze
 
+  # @param [String] payload The MARCXML returned by the Aleph API
+  # @return [String]
   def self.strip_wrapper(payload)
     payload.sub(PAYLOAD_HEADER, '').sub(PAYLOAD_FOOTER, '')
   end
 
+  # @param [String] xml_file_name The path to a ProQuest XML file
+  # @return [String]
   def self.parse_file(xml_file_name)
     binary_filename = extract_binary_filename(xml_file_name)
     result = query_pegasus(binary_filename)
