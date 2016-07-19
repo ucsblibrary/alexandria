f9eaa71d12d53c31ca7d8d02f9d7ca1c1b5d6807
val99erie <val99erie@gmail.com>
2016-07-19 11:05:17 -0500

    Fixed a problem with punctuation in data

--- lib/traject/extract_contributors.rb
+++ lib/traject/extract_contributors.rb
@@ -59,7 +59,8 @@ module ExtractContributors
   end
 
   def roles_for(field)
-    sub_4 = field.subfields.select { |s| s.code == '4'.freeze }.map(&:value)
+    sub_4 = field.subfields.select { |s| s.code == '4'.freeze }
+              .map { |role| Traject::Macros::Marc21.trim_punctuation(role.value) }
     sub_e = field.subfields.select { |s| s.code == 'e'.freeze }
               .map { |role| Traject::Macros::Marc21.trim_punctuation(role.value) }
     roles = sub_4 + sub_e
