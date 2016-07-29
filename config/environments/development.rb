7e71abb621148091df7f6fe1f6e65f3136036b41
val99erie <val99erie@gmail.com>
2016-07-29 11:31:09 -0500

    Allow config of hostname in dev env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,5 +39,5 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.host_name = 'localhost'
+  config.host_name = ENV['DEV_HOST_NAME'] || 'localhost'
 end
