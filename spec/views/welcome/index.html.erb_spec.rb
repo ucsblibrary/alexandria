bf405116cbd00ec491b96f883faa9a6ec3dfea43
Alex Dunn <adunn@ucsb.edu>
2016-08-03 08:23:03 -0700

    [test] fix homepage view spec

--- spec/views/welcome/index.html.erb_spec.rb
+++ spec/views/welcome/index.html.erb_spec.rb
@@ -1,8 +1,9 @@
+# coding: utf-8
 require 'rails_helper'
 
 RSpec.describe 'welcome/index.html.erb', type: :view do
   it 'has a link to search for ETDs' do
     render
-    expect(rendered).to have_link 'electronic theses and dissertations', href: search_catalog_path('f[active_fedora_model_ssi][]' => 'ETD')
+    expect(rendered).to have_link('Read more…', href: about_path)
   end
 end
