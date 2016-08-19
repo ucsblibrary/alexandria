9c94d65fbd8111cb4588cbff1fe3f2cce33c6968
Alex Dunn <adunn@ucsb.edu>
2016-08-19 10:28:35 -0700

    retain backtraces for dev env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
