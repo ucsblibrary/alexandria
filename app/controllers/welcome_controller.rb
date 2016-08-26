c7f35756e15b1f01c0bbe735ec114308fc4387a8
Alex Dunn <adunn@ucsb.edu>
2016-08-25 17:08:52 -0700

    Full bleed home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,8 +3,19 @@ class WelcomeController < ApplicationController
   layout 'blacklight', except: :index
 
   def index
+    @background = images.sample
+
+    port = ':3000' unless Rails.env.production?
+    size = 2000
+    @background['url'] = "http://#{Rails.application.config.host_name}#{port}"\
+                         "/images/#{@background['image_path']}"\
+                         "/#{@background['region']}/#{size},/0/default.jpg"
   end
 
   def about
+  end
+
+  def images
+    @images ||= YAML.load(File.read(Rails.root.join('config', 'homepage.yml')))
   end
 end
