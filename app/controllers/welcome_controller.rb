f4546705a63047ebaef7c26f6a9567fae3090cab
Alex Dunn <adunn@ucsb.edu>
2016-09-02 09:20:52 -0700

    WelcomeController: fix object URL
    Also simplify @background logic

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,11 +5,14 @@ class WelcomeController < ApplicationController
   def index
     @background = images.sample
 
+    @object_url = Rails.application.secrets.ezid_default_shoulder.sub(%r{\/.{3}$}, '') +
+                  "/#{@background['id']}"
+
     port = ':3000' unless Rails.env.production?
     size = 2000
-    @background['url'] = "//#{Rails.application.config.host_name}#{port}"\
-                         "/images/#{@background['image_path']}"\
-                         "/#{@background['region']}/#{size},/0/default.jpg"
+    @background_url = "//#{Rails.application.config.host_name}#{port}"\
+                      "/images/#{@background['image_path']}"\
+                      "/#{@background['region']}/#{size},/0/default.jpg"
   end
 
   def about
