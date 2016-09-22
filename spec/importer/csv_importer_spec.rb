d89ce9d6506dd48e0f806a10bd0a1febc777c992
Alex Dunn <adunn@ucsb.edu>
2016-09-22 14:25:16 -0700

    [test] update CSV importer spec for refactor

--- spec/importer/csv_importer_spec.rb
+++ spec/importer/csv_importer_spec.rb
@@ -2,39 +2,22 @@ require 'rails_helper'
 require 'importer'
 
 describe Importer::CSV do
+  before { Image.destroy_all }
+
   let(:data) { Dir['spec/fixtures/images/*'] }
 
   context 'when the model is specified' do
-    let(:csv_file) { "#{fixture_path}/csv/pamss045_with_type.csv" }
+    let(:metadata) { ["#{fixture_path}/csv/pamss045_with_type.csv"] }
 
-    before do
-      Image.destroy_all
-    end
-
-    before, after = nil
     it 'creates a new image' do
-      head, tail = Importer::CSV.split(csv_file)
-      expect(head.first).to eq('type')
-      expect(tail.length).to eq(2)
-
-      tail.each do |row|
-        attrs = Importer::CSV.csv_attributes(head, row)
-        files = if attrs[:files].nil?
-                  []
-                else
-                  data.select { |d| attrs[:files].include? File.basename(d) }
-                end
-        VCR.use_cassette('csv_importer') do
-          Importer::CSV.import(
-            attributes: attrs,
-            files: files
-          )
-        end
+      VCR.use_cassette('csv_importer') do
+        Importer::CSV.import(
+          metadata, data, { skip: 0, verbose: false }
+        )
       end
-
       expect(Image.count).to eq(1)
-      img = Image.first
 
+      img = Image.first
       expect(img.title).to eq ['Dirge for violin and piano (violin part)']
       expect(img.file_sets.count).to eq 4
       expect(img.file_sets.map { |d| d.files.map(&:file_name) }.flatten)
