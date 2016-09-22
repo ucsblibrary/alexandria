6e766fe449584d147632e6aba69f22adf2bd517a
Alex Dunn <adunn@ucsb.edu>
2016-09-22 16:01:46 -0700

    [test] update MODS spec for refactor

--- spec/importer/mods_importer_spec.rb
+++ spec/importer/mods_importer_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 require 'importer'
 
 describe Importer::MODS do
-  let(:images) { Dir['spec/fixtures/images/cusbspcmss36*.tif'] }
+  let(:data) { Dir['spec/fixtures/images/cusbspcmss36*.tif'] }
+  let(:metadata) { ['spec/fixtures/mods/cusbspcmss36_110108.xml'] }
 
   before do
     # Don't fetch external records during specs
@@ -10,53 +11,65 @@ describe Importer::MODS do
   end
 
   describe '#import an Image' do
-    before do
-      Collection.destroy_all
-      if ActiveFedora::Base.exists? 'fk41234567'
-        ActiveFedora::Base.find('fk41234567').destroy(eradicate: true)
-      end
-      if ActiveFedora::Base.exists? 'fk49876543'
-        ActiveFedora::Base.find('fk49876543').destroy(eradicate: true)
-      end
-    end
-    let(:file) { 'spec/fixtures/mods/cusbspcmss36_110108.xml' }
+    context 'without an existing collection' do
+      before do
+        VCR.use_cassette('mods_importer') do
+          if ActiveFedora::Base.exists? 'fk41234567'
+            ActiveFedora::Base.find('fk41234567').destroy(eradicate: true)
+          end
+          if ActiveFedora::Base.exists? 'fk49876543'
+            ActiveFedora::Base.find('fk49876543').destroy(eradicate: true)
+          end
 
-    it 'creates a new image, files, and a collection' do
-      image = nil
-
-      expect do
-        VCR.use_cassette('mods_importer', record: :new_episodes) do
-          image = Importer::MODS.import(file, images)
+          Collection.destroy_all
+          Image.destroy_all
+          FileSet.destroy_all
         end
-      end.to change { Image.count }.by(1)
-        .and change { FileSet.count }.by(2)
-        .and change { Collection.count }.by(1)
+      end
 
-      original = image.file_sets.first.original_file
-      expect(original.mime_type).to eq 'image/tiff'
-      expect(original.original_name).to eq 'cusbspcmss36_110108_1_a.tif'
+      it 'creates a new image, files, and a collection' do
+        VCR.use_cassette('mods_importer') do
+          Importer::MODS.import(metadata, data, skip: 0, verbose: false)
+        end
 
-      # Image.reload doesn't clear @file_association
-      reloaded = Image.find(image.id)
-      expect(reloaded.file_sets.first).not_to be_nil
+        expect(Image.count).to eq 1
+        expect(FileSet.count).to eq 2
+        expect(Collection.count).to eq 1
 
-      expect(reloaded.identifier.first).to match %r{^ark:/99999/fk4\w{7}$}
+        image = Image.first
+        original = image.file_sets.first.original_file
+        expect(original.mime_type).to eq 'image/tiff'
+        expect(original.original_name).to eq 'cusbspcmss36_110108_1_a.tif'
 
-      expect(reloaded.admin_policy_id).to eq AdminPolicy::PUBLIC_POLICY_ID
+        # Image.reload doesn't clear @file_association
+        reloaded = Image.find(image.id)
+        expect(reloaded.file_sets.first).not_to be_nil
 
-      coll = reloaded.in_collections.first
-      expect(coll.accession_number).to eq ['SBHC Mss 36']
-      expect(coll.title).to eq ['Santa Barbara picture postcards collection']
-      expect(coll.members).to eq [reloaded]
-      expect(coll.admin_policy_id).to eq AdminPolicy::PUBLIC_POLICY_ID
+        expect(reloaded.identifier.first).to match %r{^ark:/99999/fk4\w{7}$}
 
-      solr_doc = ActiveFedora::SolrService.query("id:#{image.id}").first
-      expect(solr_doc['collection_label_ssim']).to eq ['Santa Barbara picture postcards collection']
+        expect(reloaded.admin_policy_id).to eq AdminPolicy::PUBLIC_POLICY_ID
+
+        coll = reloaded.in_collections.first
+        expect(coll.accession_number).to eq ['SBHC Mss 36']
+        expect(coll.title).to eq ['Santa Barbara picture postcards collection']
+        expect(coll.members).to eq [reloaded]
+        expect(coll.admin_policy_id).to eq AdminPolicy::PUBLIC_POLICY_ID
+
+        solr_doc = ActiveFedora::SolrService.query("id:#{image.id}").first
+        expect(solr_doc['collection_label_ssim']).to eq ['Santa Barbara picture postcards collection']
+      end
     end
 
     context 'when the collection already exists' do
       before do
-        Collection.destroy_all
+        if ActiveFedora::Base.exists? 'fk41234567'
+          ActiveFedora::Base.find('fk41234567').destroy(eradicate: true)
+        end
+        if ActiveFedora::Base.exists? 'fk49876543'
+          ActiveFedora::Base.find('fk49876543').destroy(eradicate: true)
+        end
+
+        Collection.all.each { |c| c.destroy(eradicate: true) }
 
         # skip creating files
         allow_any_instance_of(Importer::Factory::ImageFactory).to receive(:after_create)
@@ -67,8 +80,8 @@ describe Importer::MODS do
         expect(coll.members.size).to eq 0
 
         expect do
-          VCR.use_cassette('mods_importer', record: :new_episodes) do
-            Importer::MODS.import(file, images)
+          VCR.use_cassette('mods_importer') do
+            Importer::MODS.import(metadata, data, skip: 0, verbose: false)
           end
         end.to change { Collection.count }.by(0)
 
@@ -78,45 +91,51 @@ describe Importer::MODS do
   end
 
   describe '#import a Collection' do
-    before do
-      Collection.destroy_all
-    end
-    let(:file) { 'spec/fixtures/mods/sbhcmss78_FlyingAStudios_collection.xml' }
+    let(:metadata) { ['spec/fixtures/mods/sbhcmss78_FlyingAStudios_collection.xml'] }
 
-    it 'creates a collection' do
-      coll = nil
-      expect do
-        # New cassette to avoid ActiveFedora::IllegalOperation:
-        #                         Attempting to recreate existing ldp_source
-        VCR.use_cassette('mods_importer-1', record: :new_episodes) do
-          coll = Importer::MODS.import(file, [])
-        end
-      end.to change { Collection.count }.by(1).and change {
-        Person.count
-      }.by(1)
+    context 'when the collection does not exist' do
+      before do
+        Collection.all.each { |c| c.destroy(eradicate: true) }
+        Image.all.each { |i| i.destroy(eradicate: true) }
+      end
 
-      expect(coll.id).to match(/^fk4\w{7}$/)
-      expect(coll.accession_number).to eq ['SBHC Mss 78']
-      expect(coll.title).to eq ['Joel Conway / Flying A Studio photograph collection']
-      expect(coll.admin_policy_id).to eq AdminPolicy::PUBLIC_POLICY_ID
+      it 'creates a collection' do
+        expect do
+          VCR.use_cassette('mods_importer') do
+            Importer::MODS.import(metadata, data, skip: 0, verbose: false)
+          end
+        end.to change { Collection.count }.by(1).and change { Person.count }.by(1)
 
-      expect(coll.collector.count).to eq 1
-      uri = coll.collector.first.rdf_subject.value
-      collector = Person.find(Person.uri_to_id(uri))
-      expect(collector.foaf_name).to eq 'Conway, Joel'
+        coll = Collection.first
+
+        expect(coll.id).to match(/^fk4\w{7}$/)
+        expect(coll.accession_number).to eq ['SBHC Mss 78']
+        expect(coll.title).to eq ['Joel Conway / Flying A Studio photograph collection']
+        expect(coll.admin_policy_id).to eq AdminPolicy::PUBLIC_POLICY_ID
+
+        expect(coll.collector.count).to eq 1
+        uri = coll.collector.first.rdf_subject.value
+        collector = Person.find(Person.uri_to_id(uri))
+        expect(collector.foaf_name).to eq 'Conway, Joel'
+      end
     end
 
     context 'when the collection already exists' do
+      before do
+        Collection.all.each { |c| c.destroy(eradicate: true) }
+        Image.all.each { |i| i.destroy(eradicate: true) }
+      end
+
       let!(:existing) { Collection.create!(accession_number: ['SBHC Mss 78'], title: ['Test Collection']) }
 
       it 'it adds metadata to existing collection' do
-        coll = nil
         expect do
-          VCR.use_cassette('mods_importer', record: :new_episodes) do
-            coll = Importer::MODS.import(file, [])
+          VCR.use_cassette('mods_importer') do
+            Importer::MODS.import(metadata, data, skip: 0, verbose: false)
           end
         end.to change { Collection.count }.by(0)
 
+        coll = Collection.first
         expect(coll.id).to eq existing.id
         expect(coll.accession_number).to eq ['SBHC Mss 78']
         expect(coll.title).to eq ['Joel Conway / Flying A Studio photograph collection']
@@ -125,20 +144,19 @@ describe Importer::MODS do
 
     context 'when the person already exists' do
       before do
+        Collection.all.each { |c| c.destroy(eradicate: true) }
         Person.destroy_all
       end
       let!(:existing) { Person.create(foaf_name: 'Conway, Joel') }
 
       it "doesn't create another person" do
-        coll = nil
         expect do
-          VCR.use_cassette('mods_importer-2', record: :new_episodes) do
-            coll = Importer::MODS.import(file, [])
+          VCR.use_cassette('mods_importer') do
+            Importer::MODS.import(metadata, data, skip: 0, verbose: false)
           end
-        end.to change { Collection.count }.by(1).and change {
-          Person.count
-        }.by(0)
+        end.to change { Collection.count }.by(1).and change { Person.count }.by(0)
 
+        coll = Collection.first
         expect(coll.collector.count).to eq 1
         uri = coll.collector.first.rdf_subject.value
         collector = Person.find(Person.uri_to_id(uri))
@@ -148,7 +166,7 @@ describe Importer::MODS do
   end
 
   describe 'fields that have Strings instead of URIs' do
-    let(:file) { 'spec/fixtures/mods/sbhcmss78_FlyingAStudios_collection.xml' }
+    let(:metadata) { ['spec/fixtures/mods/sbhcmss78_FlyingAStudios_collection.xml'] }
 
     let(:frodo) { 'Frodo Baggins' }
     let(:bilbo) { 'Bilbo Baggins' }
@@ -163,8 +181,8 @@ describe Importer::MODS do
 
       it 'finds or creates the rights holders' do
         expect do
-          VCR.use_cassette('mods_importer-3', record: :new_episodes) do
-            Importer::MODS.import(file, images)
+          VCR.use_cassette('mods_importer') do
+            Importer::MODS.import(metadata, data, skip: 0, verbose: false)
           end
         end.to change { Agent.exact_model.count }.by(1)
 
