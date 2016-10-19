31ea8fdd86327c0caaef9aee728cf9b385286610
val99erie <val99erie@gmail.com>
2016-10-19 10:13:03 -0500

    Work-around for Rails crash in production mode.  See this issue:  https://github.com/projecthydra/curation_concerns/issues/1045

---
+++ app/models/minter_state.rb
@@ -0,0 +1,15 @@
+# This is a work-around to allow us to run Alexandria in
+# production mode.  If this file doesn't exist, we get this
+# error when running rails server or rails console in
+# production mode:
+# activesupport-4.2.6/lib/active_support/dependencies.rb:274:in `require': No such file to load -- minter_state (LoadError)
+#
+# TODO:  Delete this file once this issue is fixed.
+#
+# See this:
+# https://github.com/projecthydra-labs/active_fedora-noid/issues/29
+#
+# See also:
+# https://github.com/projecthydra/curation_concerns/issues/1045
+
+# class MinterState; end
