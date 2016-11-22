57ead9981f8157ad9fe75eb8135a6c6d7d65344d
Alex Dunn <dunn.alex@gmail.com>
2016-11-21 16:43:36 -0800

    fix tests

--- spec/features/create_record_spec.rb
+++ spec/features/create_record_spec.rb
@@ -10,8 +10,7 @@ feature 'Record Creation:' do
     end
 
     scenario 'creates a new record' do
-      visit search_catalog_path
-      click_link 'Create a new record'
+      visit new_record_path
       select 'Group', from: 'type'
       click_button 'Next'
       title = 'My New Group'
