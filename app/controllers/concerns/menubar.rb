b2fd294c7a06680c754f105b399ea8e60f297c24
val99erie <val99erie@gmail.com>
2016-12-08 10:40:07 -0600

    Admin nav links

--- app/controllers/concerns/menubar.rb
+++ app/controllers/concerns/menubar.rb
@@ -1,4 +1,11 @@
 module Menubar
+  extend ActiveSupport::Concern
+
+  included do
+    helper_method :embargo_manager?
+    helper_method :can_read_authorities?
+  end
+
   def embargo_manager?
     can?(:discover, Hydra::AccessControls::Embargo)
   end
@@ -10,4 +17,5 @@ module Menubar
   def can_destroy_authorities?
     can?(:destroy, :local_authorities)
   end
+
 end
