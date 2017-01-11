68489f70339acc4eb0211dd7a18ecc4003efea6f
Tom Johnson <tom@dp.la>
2017-01-11 10:34:55 -0800

    [test] make csv_importer_spec order agnostic
    The order of files in a fileset is not stable on RDF.rb 2.0 and above. I
    ran into this test failing as I was testing CurationConcerns 2.0 RC1,
    and thought I'd generalize it quickly.
    
    Closes #249.

--- spec/importer/csv_importer_spec.rb
+++ spec/importer/csv_importer_spec.rb
@@ -21,7 +21,10 @@ describe Importer::CSV do
       expect(img.title).to eq ['Dirge for violin and piano (violin part)']
       expect(img.file_sets.count).to eq 4
       expect(img.file_sets.map { |d| d.files.map(&:file_name) }.flatten)
-        .to eq(['dirge1.tif', 'dirge2.tif', 'dirge3.tif', 'dirge4.tif'])
+        .to contain_exactly('dirge1.tif', 
+                            'dirge2.tif', 
+                            'dirge3.tif', 
+                            'dirge4.tif')
       expect(img.in_collections.first.title).to eq(['Mildred Couper papers'])
       expect(img.license.first.rdf_label).to eq ['In Copyright']
     end
