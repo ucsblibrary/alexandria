7197f574dcec6c28515c6e84734b0893d0ae8191
Alex Dunn <dunn.alex@gmail.com>
2017-02-21 09:28:56 -0800

    [test] add FileSet routing spec

---
+++ spec/routing/file_set_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+describe "FileSet routes" do
+  let(:id) { "fk41234567" }
+
+  before do
+    ActiveFedora::Base.find(id).destroy(eradicate: true) if ActiveFedora::Base.exists? id
+    FileSet.create!(id: id)
+  end
+
+  context "download path" do
+    it "finds the correct controller" do
+      expect(get: Rails.application.routes.url_helpers.download_url(id, host: ApplicationHelper.hostp))
+        .to route_to(
+          controller: "downloads",
+          action: "show",
+          id: id
+        )
+    end
+  end
+end
