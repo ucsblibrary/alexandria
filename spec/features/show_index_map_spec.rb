b130ad8974aca4a899b16dc910c99a7f208612d5
val99erie <val99erie@gmail.com>
2017-03-08 10:37:58 -0600

    Add thumbnails to IndexMap show page. DIGREPO-740

--- spec/features/show_index_map_spec.rb
+++ spec/features/show_index_map_spec.rb
@@ -7,10 +7,41 @@ feature "IndexMap show page:" do
   let(:title) { ["Region around the North Pole : giving the records of the most important explorations"] }
   let(:extent) { ["1 map : color ; 27 x 38 cm"] }
   let(:scale) { ["1:18,374,400"] }
+
+  # This is the map whose show page we want to view
   let(:map) do
     VCR.use_cassette("show_index_map_feature_spec") do
-      FactoryGirl.create(:public_index_map, title: title, creator: [creator_uri], extent: extent, scale: scale)
+      FactoryGirl.create(:public_index_map, title: title, creator: [creator_uri], extent: extent, scale: scale, parent_id: map_set.id)
     end
+  end
+
+  let!(:sibling_index_map) do
+    FactoryGirl.create(:public_index_map, title: ["Sibling Index Map"], parent_id: map_set.id)
+  end
+
+  let!(:map_set) do
+    FactoryGirl.create(:public_map_set, title: ["Parent Map Set"])
+  end
+
+  let!(:component_map) do
+    FactoryGirl.create(:public_component_map, title: ["CM0"], parent_id: map_set.id)
+  end
+
+  let(:file_path) { File.join(fixture_path, "maps", "7070s_250_u54_index.jpg") }
+  let(:file_set) do
+    FileSet.new(admin_policy_id: AdminPolicy::PUBLIC_POLICY_ID) do |fs|
+      Hydra::Works::AddFileToFileSet.call(fs, File.new(file_path), :original_file)
+    end
+  end
+
+  before do
+    map.members << file_set
+    map.save!
+    sibling_index_map.members << file_set
+    sibling_index_map.save!
+    component_map.members << file_set
+    component_map.save!
+    map_set.update_index
   end
 
   scenario "show the page" do
@@ -19,5 +50,9 @@ feature "IndexMap show page:" do
     expect(page).to have_content extent.first
     expect(page).to have_content "Century Company"
     expect(page).to have_content scale.first
+
+    expect(page).to have_link("indexmap0")
+    expect(page).to have_link("indexmap1")
+    expect(page).to have_link("componentmap0")
   end
 end
