16c194cc1c2ec3e9c948a891f4f663b301d20b86
val99erie <val99erie@gmail.com>
2017-03-10 10:09:56 -0600

    Get the full list of child maps, not limited by solr default of 10 rows.

--- app/indexers/map_set_indexer.rb
+++ app/indexers/map_set_indexer.rb
@@ -5,10 +5,10 @@ class MapSetIndexer < ObjectIndexer
   def generate_solr_document
     super do |solr_doc|
       query = ActiveFedora::SolrQueryBuilder.construct_query(parent_id_ssim: object.id)
-      results = ActiveFedora::SolrService.query(query, fl: "id has_model_ssim", sort: MAP_SORT_FIELD)
+      results = ActiveFedora::SolrService.query(query, fl: "id has_model_ssim", sort: MAP_SORT_FIELD, rows: ActiveFedora::Base.count)
       index_maps, component_maps = results.partition { |doc| doc["has_model_ssim"].include? "IndexMap" }
-      solr_doc["index_maps_ssim"] = index_maps.map { |doc| doc["id"] }
-      solr_doc["component_maps_ssim"] = component_maps.map { |doc| doc["id"] }
+      solr_doc["index_maps_ssim"] = index_maps.map(&:id)
+      solr_doc["component_maps_ssim"] = component_maps.map(&:id)
       solr_doc[ISSUED] = issued
       solr_doc[COPYRIGHTED] = display_date("date_copyrighted")
       solr_doc["rights_holder_label_tesim"] = object["rights_holder"].flat_map(&:rdf_label)
---
+++ spec/indexers/map_set_indexer_spec.rb
@@ -0,0 +1,45 @@
+require "rails_helper"
+
+describe MapSetIndexer do
+  let(:map_set) { create(:public_map_set) }
+
+  # Having more than 10 component or index maps is significant
+  # because by default a solr query will only return 10 records.
+  # If you want the full set of records, you have to explictly
+  # tell solr the number of rows to return.
+  context "a map set with more than 10 component maps" do
+    let(:cm_docs) do
+      docs = []
+      for i in 1..11 do
+        docs << SolrDocument.new(id: i.to_s,
+                                 parent_id_ssim: map_set.id,
+                                 has_model_ssim: "ComponentMap")
+      end
+      docs
+    end
+
+    let(:im_docs) do
+      docs = []
+      for i in 1..11 do
+        docs << SolrDocument.new(id: (100 + i).to_s,
+                                 parent_id_ssim: map_set.id,
+                                 has_model_ssim: "IndexMap")
+      end
+      docs
+    end
+
+    before do
+      ActiveFedora::SolrService.add(cm_docs + im_docs)
+      ActiveFedora::SolrService.commit
+    end
+
+    context "#generate_solr_document" do
+      subject { described_class.new(map_set).generate_solr_document }
+
+      it "returns the full list of component and index maps" do
+        expect(subject["component_maps_ssim"]).to eq cm_docs.map(&:id)
+        expect(subject["index_maps_ssim"]).to eq im_docs.map(&:id)
+      end
+    end
+  end
+end
