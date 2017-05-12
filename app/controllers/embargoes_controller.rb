7a03588414fdf86823bd896fe4a98fe4daa38589
Alex Dunn <dunn.alex@gmail.com>
2017-05-12 11:29:45 -0700

    fix layout for embargo page, broken by 6c4bcdeb
    - also fix local variable shadowing
    
    Closes GH-424.

--- app/controllers/embargoes_controller.rb
+++ app/controllers/embargoes_controller.rb
@@ -3,6 +3,8 @@
 # TODO: Merge this with CurationConcerns
 # Provides a mechanism to interact with embargos.
 class EmbargoesController < ApplicationController
+  layout "curation_concerns"
+
   include CurationConcerns::Collections::AcceptsBatches
   include Hydra::Controller::ControllerBehavior
 
@@ -26,9 +28,9 @@ class EmbargoesController < ApplicationController
   def update
     filter_docs_with_rights_access!
     copy_visibility = params.fetch(:embargoes, {}).values.map { |h| h[:copy_visibility] }
-    ActiveFedora::Base.find(batch).each do |curation_concern|
-      CurationConcerns::Actors::EmbargoActor.new(curation_concern).destroy
-      curation_concern.copy_admin_policy_to_files! if copy_visibility.include?(curation_concern.id)
+    ActiveFedora::Base.find(batch).each do |cc|
+      CurationConcerns::Actors::EmbargoActor.new(cc).destroy
+      cc.copy_admin_policy_to_files! if copy_visibility.include?(cc.id)
     end
     redirect_to embargoes_path
   end
