52dd2bf324489db04518cefbe06826f4909543ed
Alex Dunn <dunn.alex@gmail.com>
2017-05-18 11:57:44 -0700

    [test] other works might exist during ExpireEmbargos spec

--- spec/lib/expire_embargo_spec.rb
+++ spec/lib/expire_embargo_spec.rb
@@ -4,7 +4,13 @@ require "rails_helper"
 require "expire_embargos"
 
 describe ExpireEmbargos do
-  before { AdminPolicy.ensure_admin_policy_exists }
+  before do
+    AdminPolicy.ensure_admin_policy_exists
+  end
+
+  let(:visibility_during_embargo) { RDF::URI(ActiveFedora::Base.id_to_uri("authorities/policies/restricted")) }
+  let(:visibility_after_embargo) { RDF::URI(ActiveFedora::Base.id_to_uri("authorities/policies/public")) }
+
   let!(:work) do
     create(:etd,
            visibility_during_embargo: visibility_during_embargo,
@@ -12,11 +18,8 @@ describe ExpireEmbargos do
            embargo_release_date: 10.days.ago.to_datetime)
   end
 
-  let(:visibility_during_embargo) { RDF::URI(ActiveFedora::Base.id_to_uri("authorities/policies/restricted")) }
-  let(:visibility_after_embargo) { RDF::URI(ActiveFedora::Base.id_to_uri("authorities/policies/public")) }
-
   it "clears the embargo" do
-    expect(described_class.run).to eq 1
+    described_class.run
     expect(work.reload.admin_policy_id).to eq "authorities/policies/public"
   end
 end
