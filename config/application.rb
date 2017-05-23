5ca51c413dd33184e627ad963cf46e38331ec134
Alex Dunn <dunn.alex@gmail.com>
2017-05-23 10:49:33 -0700

    config/application: use parameter filtering

--- config/application.rb
+++ config/application.rb
@@ -17,9 +17,6 @@ module Alexandria
       g.test_framework :rspec, spec: true
     end
 
-    # FIXME: https://gitlab.com/gitlab-org/gitlab-ce/merge_requests/5555/diffs#note_15843755
-    config.action_controller.permit_all_parameters = true
-
     # Handle exceptions manually
     config.exceptions_app = routes
 
