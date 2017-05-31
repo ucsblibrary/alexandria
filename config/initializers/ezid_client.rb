44745adb49e847f5280cdfa3d1e95c85eb2374b2
Alex Dunn <dunn.alex@gmail.com>
2017-05-31 16:10:36 -0700

    config: ezid.yml is redundant
    * the values we actually customize are all in secrets.yml
    
    Closes GH-450.

--- config/initializers/ezid_client.rb
+++ config/initializers/ezid_client.rb
@@ -1,26 +1,8 @@
 # frozen_string_literal: true
 
-file = Rails.root.join("config", "application.yml")
-raise "You are missing a configuration file: #{file}." unless File.exist?(file)
-
-begin
-  yml = YAML.load_file(file)
-rescue StandardError
-  raise("#{file} was found, but could not be parsed.\n")
-end
-
-if yml.nil? || !yml.is_a?(Hash)
-  raise("#{file} was found, but was blank or malformed.\n")
-end
-
-options = yml.fetch(Rails.env).with_indifferent_access
-
 Ezid::Client.configure do |config|
-  config.user             = Rails.application.secrets.ezid_user
-  config.password         = Rails.application.secrets.ezid_pass
-  config.host             = options.fetch(:ezid_host)
-  config.port             = options.fetch(:ezid_port)
-  config.use_ssl          = (options.fetch(:ezid_port) != 80)
   config.default_shoulder = Rails.application.secrets.ezid_default_shoulder
   config.logger           = Rails.logger
+  config.password         = Rails.application.secrets.ezid_pass
+  config.user             = Rails.application.secrets.ezid_user
 end
