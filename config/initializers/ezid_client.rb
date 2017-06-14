914de722dc4bedfa315acf6ee5486693c871ad52
Alex Dunn <dunn.alex@gmail.com>
2017-06-14 09:34:54 -0700

    Revert "config: put Ezid shoulder in application.yml"
    This reverts commit 929e61db25adbacfdedff91709bff21a363f6f98.

--- config/initializers/ezid_client.rb
+++ config/initializers/ezid_client.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Ezid::Client.configure do |config|
-  config.default_shoulder = Settings.ezid_shoulder
+  config.default_shoulder = Rails.application.secrets.ezid_default_shoulder
   config.logger           = Rails.logger
   config.password         = Rails.application.secrets.ezid_pass
   config.user             = Rails.application.secrets.ezid_user
