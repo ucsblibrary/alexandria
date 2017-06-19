a3c0df8cf14b46b4c16e391d16434d73735cb7b1
Alex Dunn <dunn.alex@gmail.com>
2017-06-18 19:48:40 -0700

    traject/extract_notes: fix Metrics/AbcSize violation

--- lib/traject/extract_notes.rb
+++ lib/traject/extract_notes.rb
@@ -2,26 +2,24 @@
 
 module ExtractNotes
   def extract_notes
-    basic_ext = Traject::MarcExtractor.new("500a:590a", separator: '\n\n')
-    performer_ext = Traject::MarcExtractor.new("511a")
-    venue_ext = Traject::MarcExtractor.new("518abop3")
-    owner_ext = Traject::MarcExtractor.new("561a")
-
     lambda do |record, accumulator|
-      accumulator << basic_ext.extract(record).compact
+      accumulator << [
+        Traject::MarcExtractor.new(
+          "500a:590a", separator: '\n\n'
+        ).extract(record).compact,
 
-      accumulator << (performer_ext.extract(record).map do |performer|
-                        { type: :performer, name: performer }
-                      end)
+        Traject::MarcExtractor.new("511a").extract(record).map do |performer|
+          { type: :performer, name: performer }
+        end,
 
-      accumulator << (venue_ext.extract(record).map do |venue|
-                        { type: :venue, name: venue }
-                      end)
+        Traject::MarcExtractor.new("518abop3").extract(record).map do |venue|
+          { type: :venue, name: venue }
+        end,
 
-      accumulator << (owner_ext.extract(record).map do |owner|
-                        { type: :ownership, name: owner }
-                      end)
-
+        Traject::MarcExtractor.new("561a").extract(record).map do |owner|
+          { type: :ownership, name: owner }
+        end,
+      ]
       accumulator.flatten!
     end
   end
