8b36bc5eed9f52b1fdaae6200c908eb612186d12
Alex Dunn <dunn.alex@gmail.com>
2017-06-18 20:05:25 -0700

    traject/extract_issue_date: fix Metrics/AbcSize violation

--- lib/traject/extract_issue_date.rb
+++ lib/traject/extract_issue_date.rb
@@ -5,18 +5,26 @@ module ExtractIssueDate
   def extract_issue_date
     date_extractor = Traject::MarcExtractor.new("008[7-10]")
     label_extractor = Traject::MarcExtractor.new("260c:264|*1|c")
+
     lambda do |record, accumulator|
-      date = date_extractor.extract(record).compact.first
+      attrs = create_attrs(date_extractor.extract(record).compact.first)
+
       label = label_extractor.extract(record).compact.first
-      attrs = if date.last == "u"
-                start_date = date.tr("u", "0").to_i
-                finish_date = start_date + 9
-                { start: [start_date], start_qualifier: ["approximate"],
-                  finish: [finish_date], finish_qualifier: ["approximate"], }
-              else
-                { start: date.to_i }
-              end
       accumulator << attrs.merge(label: label)
     end
+  end
+
+  def create_attrs(date)
+    return { start: date.to_i } unless date.last == "u"
+
+    start_date = date.tr("u", "0").to_i
+    finish_date = start_date + 9
+
+    {
+      start: [start_date],
+      start_qualifier: ["approximate"],
+      finish: [finish_date],
+      finish_qualifier: ["approximate"],
+    }
   end
 end
