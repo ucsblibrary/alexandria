b3d7d8d622aae176fd6d8356854a26c325fe090f
Alex Dunn <dunn.alex@gmail.com>
2017-06-18 19:44:09 -0700

    traject/extract_work_type: fix Metrics/AbcSize violation

--- lib/traject/extract_work_type.rb
+++ lib/traject/extract_work_type.rb
@@ -1,25 +1,26 @@
 # frozen_string_literal: true
 
+WORK_TYPE_MAP = {
+  "a" => RDF::URI("http://id.loc.gov/vocabulary/resourceTypes/txt"),
+  "t" => RDF::URI("http://id.loc.gov/vocabulary/resourceTypes/txt"),
+  "e" => RDF::URI("http://id.loc.gov/vocabulary/resourceTypes/car"),
+  "f" => RDF::URI("http://id.loc.gov/vocabulary/resourceTypes/car"),
+  "c" => RDF::URI("http://id.loc.gov/vocabulary/resourceTypes/not"),
+  "d" => RDF::URI("http://id.loc.gov/vocabulary/resourceTypes/not"),
+  "i" => RDF::URI("http://id.loc.gov/vocabulary/resourceTypes/aun"),
+  "j" => RDF::URI("http://id.loc.gov/vocabulary/resourceTypes/aum"),
+  "k" => RDF::URI("http://id.loc.gov/vocabulary/resourceTypes/img"),
+  "g" => RDF::URI("http://id.loc.gov/vocabulary/resourceTypes/mov"),
+  "r" => RDF::URI("http://id.loc.gov/vocabulary/resourceTypes/art"),
+  "m" => RDF::URI("http://id.loc.gov/vocabulary/resourceTypes/mul"),
+  "p" => RDF::URI("http://id.loc.gov/vocabulary/resourceTypes/mix"),
+}.freeze
+
 module ExtractWorkType
   # Transfor leader field 006 into a LOC Resource Type URI
   def extract_work_type
-    work_type = {
-      "a" => RDF::URI("http://id.loc.gov/vocabulary/resourceTypes/txt"),
-      "t" => RDF::URI("http://id.loc.gov/vocabulary/resourceTypes/txt"),
-      "e" => RDF::URI("http://id.loc.gov/vocabulary/resourceTypes/car"),
-      "f" => RDF::URI("http://id.loc.gov/vocabulary/resourceTypes/car"),
-      "c" => RDF::URI("http://id.loc.gov/vocabulary/resourceTypes/not"),
-      "d" => RDF::URI("http://id.loc.gov/vocabulary/resourceTypes/not"),
-      "i" => RDF::URI("http://id.loc.gov/vocabulary/resourceTypes/aun"),
-      "j" => RDF::URI("http://id.loc.gov/vocabulary/resourceTypes/aum"),
-      "k" => RDF::URI("http://id.loc.gov/vocabulary/resourceTypes/img"),
-      "g" => RDF::URI("http://id.loc.gov/vocabulary/resourceTypes/mov"),
-      "r" => RDF::URI("http://id.loc.gov/vocabulary/resourceTypes/art"),
-      "m" => RDF::URI("http://id.loc.gov/vocabulary/resourceTypes/mul"),
-      "p" => RDF::URI("http://id.loc.gov/vocabulary/resourceTypes/mix"),
-    }
     lambda do |record, accumulator|
-      accumulator << work_type[record.leader.slice(6)]
+      accumulator << WORK_TYPE_MAP[record.leader.slice(6)]
     end
   end
 end
