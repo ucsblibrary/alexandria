a1091ba4f4a32bbfd61537714b4ce45637182146
Alex Dunn <dunn.alex@gmail.com>
2017-06-23 14:39:21 -0700

    [rubocop] RSpec/InstanceVariable violations for CSV map importer

--- spec/lib/importer/csv_map_set_importer_spec.rb
+++ spec/lib/importer/csv_map_set_importer_spec.rb
@@ -6,33 +6,37 @@ require "importer"
 require "active_fedora/cleaner"
 
 describe Importer::CSV do
-  before :all do
+  before do
     ActiveFedora::Cleaner.clean!
     AdminPolicy.ensure_admin_policy_exists
-    logfile = "#{fixture_path}/logs/csv_map_set_import.log"
-    described_class.log_location(logfile)
-    metadata = ["#{fixture_path}/csv/fake_brazil_data.csv"]
-    data = Dir["#{fixture_path}/maps/*"]
+
+    described_class.log_location("#{fixture_path}/logs/csv_map_set_import.log")
+
     VCR.use_cassette("csv_map_set_importer") do
       described_class.import(
-        metadata, data, skip: 0
+        ["#{fixture_path}/csv/fake_brazil_data.csv"],
+        Dir["#{fixture_path}/maps/*"],
+        skip: 0
       )
     end
-    @m = MapSet.first
   end
 
   context "all maps are imported and map set is reindexed" do
+    let(:m) { MapSet.first }
+
     it "creates all expected objects" do
       expect(MapSet.count).to eq(1)
       expect(IndexMap.count).to eq(1)
       expect(ComponentMap.count).to eq(2)
     end
+
     it "attaches objects as expected" do
-      expect(@m.index_maps.size).to be(1)
-      expect(@m.component_maps.size).to be(2)
+      expect(m.index_maps.size).to be(1)
+      expect(m.component_maps.size).to be(2)
     end
+
     it "reindexes the map set after its members are imported" do
-      r = ActiveFedora::SolrService.query("id:#{@m.id}")
+      r = ActiveFedora::SolrService.query("id:#{m.id}")
       expect(r.first["index_maps_ssim"].size).to be(1)
       expect(r.first["component_maps_ssim"].size).to be(2)
     end
