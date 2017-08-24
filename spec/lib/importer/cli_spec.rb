2c4d02b522203d663eb00b9a66389bfc421c670b
Alex Dunn <dunn.alex@gmail.com>
2017-08-24 16:46:28 -0700

    [test] add specs for Importer::CLI.make_logger

--- spec/lib/importer/cli_spec.rb
+++ spec/lib/importer/cli_spec.rb
@@ -35,4 +35,29 @@ describe Importer::CLI do
       end
     end
   end
+
+  describe "::make_logger" do
+    subject do
+      described_class.make_logger(
+        output: Rails.root.join("tmp", "honk.log"),
+        level: loglevel
+      ).level
+    end
+
+    context "with non-default log level" do
+      let(:loglevel) { "Error" }
+
+      it "sets the log level" do
+        expect(subject).to eq Logger::ERROR
+      end
+    end
+
+    context "with invalid log level" do
+      let(:loglevel) { "HONK" }
+
+      it "reverts to the default log level" do
+        expect(subject).to eq Logger::INFO
+      end
+    end
+  end
 end
