80408e98cacfcec188517d9f3351b8cf03abf573
Alex Dunn <dunn.alex@gmail.com>
2017-09-18 16:36:12 -0700

    Importer: explicitly require Parse

--- lib/importer.rb
+++ lib/importer.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "parse"
+
 factory_dir = File.join(File.dirname(__FILE__), "importer", "factories")
 Dir[File.join(factory_dir, "**", "*.rb")].each do |file|
   Rails.logger.debug "Requiring #{file}"
