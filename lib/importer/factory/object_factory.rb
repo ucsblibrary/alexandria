883b2de8ec8009b661fb0803cb3006ef6a6f3386
Alex Dunn <dunn.alex@gmail.com>
2017-10-03 09:48:09 -0700

    ObjectFactory#remove_existing_filesets: reload before saving
    This avoids Ldp::Gone errors.

--- lib/importer/factory/object_factory.rb
+++ lib/importer/factory/object_factory.rb
@@ -160,8 +160,8 @@ module Importer::Factory
       object.file_sets.each do |f|
         CurationConcerns::Actors::FileSetActor.new(f, nil).destroy
       end
-      object.save
       object.reload.file_sets
+      object.save
     end
 
     # @param [Hash] attrs
