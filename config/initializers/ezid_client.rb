c707d7bcc3773ac8e9ad3ce4f015322d532c36f0
Alex Dunn <dunn.alex@gmail.com>
2017-11-17 10:21:04 -0800

    EZID credentials: use Secret Server in production

--- config/initializers/ezid_client.rb
+++ config/initializers/ezid_client.rb
@@ -1,8 +1,20 @@
 # frozen_string_literal: true
 
+# Don't run ERB on the entire file, since in non-production contexts
+# /opt/secret-server/secretserver-jconsole.jar probably won't exist so it will
+# throw an error
+def yaml_conf
+  @yaml_conf ||= YAML.safe_load(
+    File.read(Rails.root.join("config", "ezid.yml")),
+    # by default #safe_load doesn't allow aliases
+    # https://github.com/ruby/psych/blob/2884f7bf8d1bd6433babe6b7b8e4b6007e59af97/lib/psych.rb#L290
+    [], [], true
+  )[Rails.env]
+end
+
 Ezid::Client.configure do |config|
-  config.default_shoulder = Rails.application.secrets.ezid_default_shoulder
+  config.default_shoulder = yaml_conf["shoulder"]
   config.logger           = Rails.logger
-  config.password         = Rails.application.secrets.ezid_pass
-  config.user             = Rails.application.secrets.ezid_user
+  config.password         = ERB.new(yaml_conf["password"]).result
+  config.user             = ERB.new(yaml_conf["username"]).result
 end
