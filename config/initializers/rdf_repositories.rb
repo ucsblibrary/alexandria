56230bf08b2ac068ca3d29d219978c2032f1c594
Alex Dunn <dunn.alex@gmail.com>
2017-11-22 10:14:31 -0800

    secrets: move more keys to production block

--- config/initializers/rdf_repositories.rb
+++ config/initializers/rdf_repositories.rb
@@ -3,12 +3,12 @@
 require_relative "settings"
 def configure_repositories
   ActiveTriples::Repositories.clear_repositories!
-  vocab_repo = if ENV["CI"]
-                 RDF::Repository.new
-               else
+  vocab_repo = if Rails.env.production?
                  # rubocop:disable Metrics/LineLength
                  RDF::Marmotta.new("http://#{Rails.application.secrets.marmotta_host}:8080/marmotta")
                  # rubocop:enable Metrics/LineLength
+               else
+                 RDF::Repository.new
                end
   ActiveTriples::Repositories.add_repository :vocabs, vocab_repo
 end
