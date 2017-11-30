8c2f1f6f79d384bac3d6bed269805d349d086e61
Alex Dunn <dunn.alex@gmail.com>
2017-11-30 11:09:57 -0800

    EXPLAIN_PARTIALS: update for Rails 5
    See https://www.justinweiss.com/articles/rails-5-module-number-prepend-and-the-end-of-alias-method-chain/

--- config/initializers/explain.rb
+++ config/initializers/explain.rb
@@ -6,15 +6,17 @@ if ENV["EXPLAIN_PARTIALS"]
   # On Vagrant, Passenger is run under Apache, so EXPLAIN_PARTIALS must
   # be set in /etc/sysconfig/httpd; see
   # https://www.phusionpassenger.com/library/indepth/environment_variables.html
-  class ActionView::PartialRenderer
-    def render_with_explanation(*args)
-      rendered = render_without_explanation(*args).to_s
+  module RenderWithExplanation
+    def render(*args)
+      rendered = super(*args).to_s
       # Note: We haven't figured out how to get a path when @template is nil.
       start_explanation = "\n<!-- START PARTIAL #{@template.inspect} -->\n"
       end_explanation = "\n<!-- END PARTIAL #{@template.inspect} -->\n"
-      start_explanation.html_safe + rendered + end_explanation.html_safe
+      (start_explanation + rendered + end_explanation).html_safe
     end
+  end
 
-    alias_method_chain :render, :explanation
+  class ActionView::PartialRenderer
+    prepend RenderWithExplanation
   end
 end
