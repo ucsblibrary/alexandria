01acc757f341751f3d1c83a8e8fe7196ccd1c0ed
Alex Dunn <dunn.alex@gmail.com>
2018-01-04 14:22:26 -0800

    config/application: allow overriding dev/test queue

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,5 @@ class Alexandria::Application < Rails::Application
     )[Rails.env] || {}
 
   # Set the backend for running background jobs
-  config.active_job.queue_adapter = :inline
+  config.active_job.queue_adapter = ENV["RAILS_QUEUE"]&.to_sym || :inline
 end
