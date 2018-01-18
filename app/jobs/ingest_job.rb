49d36252dfe9704e8329ac6647e9bbaefe147f5d
Alex Dunn <dunn.alex@gmail.com>
2018-01-18 11:26:37 -0800

    IngestJob: ensure model is specified

--- app/jobs/ingest_job.rb
+++ app/jobs/ingest_job.rb
@@ -4,6 +4,8 @@ class IngestJob < ApplicationJob
   queue_as :ingest
 
   def perform(args)
+    raise "No model specified in #{args.inspect}" if args[:model].blank?
+
     ::Importer::Factory.for(args[:model]).new(
       args[:attrs],
       args[:files]
