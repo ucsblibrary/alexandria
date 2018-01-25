eacc863096cedd2e6e5a9f1fce3bedbdbab516cf
Alex Dunn <dunn.alex@gmail.com>
2018-01-25 10:23:52 -0800

    [spec] failing test for minimal object show page

--- spec/features/show_image_spec.rb
+++ spec/features/show_image_spec.rb
@@ -30,13 +30,25 @@ feature "Image show page:" do
            title: title)
   end
 
+  let(:image_without_collection) do
+    create(:image,
+           admin_policy_id: AdminPolicy::PUBLIC_POLICY_ID,
+           title: title)
+  end
+
   # See config/routes.rb: routing for images is a little weird
-  scenario "show the page" do
+  scenario "show the page for the regular image" do
     visit catalog_ark_path("ark:", "99999", image.id)
 
     expect(page).to have_content extent.first
     expect(page).to have_content creator.first
     expect(page).to have_content scale.first
+    expect(page).to have_content title.first
+  end
+
+  scenario "show the page for the image missing a collection" do
+    visit catalog_ark_path("ark:", "99999", image_without_collection.id)
+
     expect(page).to have_content title.first
   end
 end
