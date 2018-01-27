ucsblibrary/alexandria

commit 4a477bcba3044470871612c2b57844d726c28c86

ingest: mark RDF URLs instead of interning all URLs in ObjectFactory

lib/parse/csv.rb
@@ -144,7 +144,12 @@ module Parse::CSV
   def self.extract_multi_value_field(header, val, processed, key = nil)
     key ||= header.to_sym
     processed[key] ||= []
-    processed[key] << val.strip
+    value = if ::Fields::URI.looks_like_uri?(val.strip)
+              { _rdf: [val.strip] }
+            else
+              val.strip
+            end
+    processed[key] << value
   end
 
   # Fields that have an associated *_type column

lib/importer/factory/object_factory.rb
@@ -18,13 +18,10 @@ module Importer::Factory
       # ActiveJob doesn't know how to serialize {RDF::URI} so we have to intern
       # them here
       @attributes = JSON.parse(attributes).map do |key, value|
-        vals = case value
-               when Array
-                 value.map do |v|
-                   ::Fields::URI.looks_like_uri?(v) ? RDF::URI.intern(v) : v
-                 end
+        vals = if value.is_a? Array
+                 value.map { |v| maybe_intern(v) }.flatten
                else
-                 value
+                 maybe_intern(value)
                end
 
         { key => vals }
@@ -32,6 +29,14 @@ module Importer::Factory
 
       @files = files
       @logger = logger
+    end
+
+    # @param value [Array, Hash]
+    # @return [Array]
+    def maybe_intern(value)
+      return value unless value.respond_to?(:key?) && value.key?("_rdf")
+
+      value["_rdf"].map { |url| RDF::URI.intern(url) }
     end
 
     def run

lib/object_factory_writer.rb
@@ -117,7 +117,7 @@ class ObjectFactoryWriter
       metadata.delete("contributors")
       metadata.delete("filename")
 
-      work_type = case metadata["work_type"].first
+      work_type = case metadata["work_type"].first["_rdf"].first
                   when *ETD_TYPES
                     "ETD"
                   when *AUDIO_TYPES

lib/parse/mods.rb
@@ -57,7 +57,8 @@ class Parse::MODS
     uri = ::Fields::MODS.resource_type(mods)[:uri]
     return [] if uri.blank?
 
-    uri.map(&:to_s)
+    # wrap in an Array to be consistent with CSV parser
+    [{ _rdf: uri.map(&:to_s) }]
   end
 
   def description
@@ -65,14 +66,14 @@ class Parse::MODS
       title: untyped_title,
       alternative: alt_title,
       description: mods_description,
-      lc_subject: subject.map(&:value),
+      lc_subject: subject,
       extent: mods.physical_description.extent.map do |node|
         strip_whitespace(node.text)
       end,
-      language: mods.language.languageTerm.valueURI,
+      language: { _rdf: mods.language.languageTerm.valueURI },
       digital_origin: mods.physical_description.digitalOrigin.map(&:text),
       publisher: mods.origin_info.publisher.map(&:text),
-      form_of_work: mods.genre.valueURI,
+      form_of_work: { _rdf: mods.genre.valueURI },
       work_type: work_type,
       citation: citation,
       notes_attributes: notes,
@@ -89,22 +90,27 @@ class Parse::MODS
       ).map { |node| strip_whitespace(node.text) },
 
       rights_holder: rights_holder,
-      copyright_status: mods.xpath(
-        "//mods:extension/copyrightStatus/@valueURI",
-        NAMESPACES
-      ).map(&:value),
 
-      license: mods.xpath(
-        "//mods:extension/copyrightStatement/@valueURI",
-        NAMESPACES
-      ).map(&:value),
+      copyright_status: {
+        _rdf: mods.xpath(
+          "//mods:extension/copyrightStatus/@valueURI",
+          NAMESPACES
+        ).map(&:value),
+      },
+
+      license: {
+        _rdf: mods.xpath(
+          "//mods:extension/copyrightStatement/@valueURI",
+          NAMESPACES
+        ).map(&:value),
+      },
     }
   end
 
   def locations
     {
-      institution: mods.location.physicalLocation.valueURI,
-      location: mods.subject.geographic.valueURI,
+      institution: { _rdf: mods.location.physicalLocation.valueURI },
+      location: { _rdf: mods.subject.geographic.valueURI },
       place_of_publication: mods.origin_info.place.placeTerm.map(&:text),
 
       sub_location: mods.location.holdingSimple.xpath(
@@ -130,10 +136,9 @@ class Parse::MODS
 
   def finding_aid
     {
-      finding_aid: mods.xpath(
-        "//mods:url[@note='Finding aid']",
-        NAMESPACES
-      ).map(&:text),
+      finding_aid: {
+        _rdf: mods.xpath("//mods:url[@note='Finding aid']", NAMESPACES).map(&:text),
+      },
     }
   end
 
@@ -188,7 +193,7 @@ class Parse::MODS
                 type: node.attributes["type"].value,
               }
             else
-              uri.value
+              { _rdf: [uri.value] }
             end
       relations[key] << val
     end
@@ -307,7 +312,12 @@ class Parse::MODS
       nodes.map do |node|
         uri = node.attributes["valueURI"]
         text = node.text
-        uri.blank? ? strip_whitespace(text) : uri.value
+
+        if uri.blank?
+          strip_whitespace(text)
+        else
+          { _rdf: [uri.value] }
+        end
       end
     end
 
@@ -320,9 +330,11 @@ class Parse::MODS
     end
 
     def subject
-      mods.xpath(
-        "//mods:subject/mods:name/@valueURI|//mods:subject/mods:topic/@valueURI",
-        NAMESPACES
-      )
+      {
+        _rdf: mods.xpath(
+          "//mods:subject/mods:name/@valueURI|//mods:subject/mods:topic/@valueURI",
+          NAMESPACES
+        ).map(&:value),
+      }
     end
 end

lib/traject/extract_language.rb
@@ -8,7 +8,7 @@ module ExtractLanguage
       abbrev = lang_extractor.extract(record).compact.first
       # Check for empty strings
       return if /\s/ =~ abbrev
-      accumulator << "http://id.loc.gov/vocabulary/iso639-2/#{abbrev}"
+      accumulator << { _rdf: ["http://id.loc.gov/vocabulary/iso639-2/#{abbrev}"] }
     end
   end
 end

lib/traject/extract_work_type.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 
 WORK_TYPE_MAP = {
-  "a" => "http://id.loc.gov/vocabulary/resourceTypes/txt",
-  "t" => "http://id.loc.gov/vocabulary/resourceTypes/txt",
-  "e" => "http://id.loc.gov/vocabulary/resourceTypes/car",
-  "f" => "http://id.loc.gov/vocabulary/resourceTypes/car",
-  "c" => "http://id.loc.gov/vocabulary/resourceTypes/not",
-  "d" => "http://id.loc.gov/vocabulary/resourceTypes/not",
-  "i" => "http://id.loc.gov/vocabulary/resourceTypes/aun",
-  "j" => "http://id.loc.gov/vocabulary/resourceTypes/aum",
-  "k" => "http://id.loc.gov/vocabulary/resourceTypes/img",
-  "g" => "http://id.loc.gov/vocabulary/resourceTypes/mov",
-  "r" => "http://id.loc.gov/vocabulary/resourceTypes/art",
-  "m" => "http://id.loc.gov/vocabulary/resourceTypes/mul",
-  "p" => "http://id.loc.gov/vocabulary/resourceTypes/mix",
+  "a" => { _rdf: ["http://id.loc.gov/vocabulary/resourceTypes/txt"] },
+  "t" => { _rdf: ["http://id.loc.gov/vocabulary/resourceTypes/txt"] },
+  "e" => { _rdf: ["http://id.loc.gov/vocabulary/resourceTypes/car"] },
+  "f" => { _rdf: ["http://id.loc.gov/vocabulary/resourceTypes/car"] },
+  "c" => { _rdf: ["http://id.loc.gov/vocabulary/resourceTypes/not"] },
+  "d" => { _rdf: ["http://id.loc.gov/vocabulary/resourceTypes/not"] },
+  "i" => { _rdf: ["http://id.loc.gov/vocabulary/resourceTypes/aun"] },
+  "j" => { _rdf: ["http://id.loc.gov/vocabulary/resourceTypes/aum"] },
+  "k" => { _rdf: ["http://id.loc.gov/vocabulary/resourceTypes/img"] },
+  "g" => { _rdf: ["http://id.loc.gov/vocabulary/resourceTypes/mov"] },
+  "r" => { _rdf: ["http://id.loc.gov/vocabulary/resourceTypes/art"] },
+  "m" => { _rdf: ["http://id.loc.gov/vocabulary/resourceTypes/mul"] },
+  "p" => { _rdf: ["http://id.loc.gov/vocabulary/resourceTypes/mix"] },
 }.freeze
 
 module ExtractWorkType

spec/lib/importer/cylinder_spec.rb
@@ -269,7 +269,7 @@ describe Importer::Cylinder do
         )
       )
 
-      expect(record1.fulltext_link.map(&:rdf_subject).map(&:to_s)).to(
+      expect(record1.fulltext_link).to(
         contain_exactly("http://www.library.ucsb.edu/OBJID/Cylinder4373",
                         "http://www.library.ucsb.edu/OBJID/Cylinder4374",
                         "http://www.library.ucsb.edu/OBJID/Cylinder4377")

spec/lib/importer/mods_spec.rb
@@ -217,7 +217,9 @@ describe Importer::MODS do
 
     let(:frodo) { "Frodo Baggins" }
     let(:bilbo) { "Bilbo Baggins" }
-    let(:pippin) { "http://id.loc.gov/authorities/names/pippin" }
+    let(:pippin) do
+      { _rdf: ["http://id.loc.gov/authorities/names/pippin"] }
+    end
 
     context "when rights_holder has strings or uris" do
       before do

commit 4fcd0840bac7e3f604de9d50ee93fe1d05eb96bd

Parse::CSV: fix typed field parsing alongside rdf hashes
The use of special RDF hashes as of
https://github.com/ucsblibrary/alexandria/commit/4a477bcba3044470871612c2b57844d726c28c86
broke our janky CSV parser for multi-value fields.

Soon this should be rewritten with a generalized metadata model parser, so I'm
comfortable with this quick fix.

lib/parse/csv.rb
@@ -82,8 +82,15 @@ module Parse::CSV
       processed[:collection] ||= {}
       update_collection(processed[:collection], Regexp.last_match(1), val)
     else
-      last_entry = Array(processed[header.to_sym]).last
-      if last_entry.is_a?(Hash) && !last_entry[:name]
+      previous_entry = Array(processed[header.to_sym]).last
+
+      # if the previous value for the named header is a non-RDF Hash that
+      # doesn't have a name key, then this is a typed field that needs to have a
+      # type assigned
+      is_typed_field = previous_entry.is_a?(Hash) &&
+                       !previous_entry.key?(:name) && !previous_entry.key?(:_rdf)
+
+      if is_typed_field
         update_typed_field(header, val, processed)
       else
         extract_multi_value_field(header, val, processed)