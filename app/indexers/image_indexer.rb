8f931bc402ba483211bee18aa67850236ee7239d
Jennifer Lindner <jenlindner@gmail.com>
2018-02-21 12:26:30 -0500

    adding openseadragon to image show pages for pan and zoom

--- app/indexers/image_indexer.rb
+++ app/indexers/image_indexer.rb
@@ -3,6 +3,7 @@
 class ImageIndexer < ObjectIndexer
   def generate_solr_document
     super do |solr_doc|
+      solr_doc["file_set_iiif_manifest_ssm"] = file_set_iiif_manifests
       solr_doc["image_url_ssm"] = file_set_images
       solr_doc["large_image_url_ssm"] = file_set_large_images
       solr_doc[ISSUED] = issued
