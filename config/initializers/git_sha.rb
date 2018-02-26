1f0c3e1a9960b25319b3ac06fdc8bb5f8a7702fb
Jamie Little <jamie@jamielittle.org>
2018-02-26 17:27:26 -0600

    Add version to footer
    This commit adds a version to the footer. This includes the
    branch or tag that the software is on along with the SHA. The
    SHA is only visible if you hover over the branch name.
    
    This is based on the instructions at:
    https://curationexperts.github.io/playbook/every_project/git_sha.html
    
    It is using this path to `revisions.log`:
    
    `/opt/alexandria/revisions.log`

---
+++ config/initializers/git_sha.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+revisions_logfile = Rails.root.parent.parent.join("revisions.log")
+
+GIT_SHA =
+  if Rails.env.production? && File.exist?(config.revisions_logfile)
+    `tail -1 #{revisions_logfile}`.chomp.split(" ")[3].gsub(/\)$/, "")
+  elsif Rails.env.development? || Rails.env.test?
+    `git rev-parse HEAD`.chomp
+  else
+    "Unknown SHA"
+  end
+
+BRANCH =
+  if Rails.env.production? && File.exist?(config.revisions_logfile)
+    `tail -1 #{revisions_logfile}`.chomp.split(" ")[1]
+  elsif Rails.env.development? || Rails.env.test?
+    `git rev-parse --abbrev-ref HEAD`.chomp
+  else
+    "Unknown branch"
+  end
+
+LAST_DEPLOYED =
+  if Rails.env.production? && File.exist?(config.revisions_logfile)
+    deployed = `tail -1 #{revisions_logfile}`.chomp.split(" ")[7]
+    Date.parse(deployed).strftime("%d %B %Y")
+  else
+    "Not in deployed environment"
+  end
