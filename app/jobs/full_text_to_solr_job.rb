17e4cad4c313311fed3a04a938dd8bc161497730
Jamie Little <jamie@jamielittle.org>
2018-03-08 11:12:12 -0600

    Index text of all attached PDFs on a work
    This commit changes the `FullTextToSolrJob` so that
    it indexes the text content of all PDFs that are attached
    to a work.
    
    This adds another PDF fixture for the updated specs.
    
    This also moves the spec for the `FullTextExtractor` class into
    `spec/services`. The class was moved, but not the spec in an
    earlier commit.
    
    Closes curationexperts/alexandria#52

--- app/jobs/full_text_to_solr_job.rb
+++ app/jobs/full_text_to_solr_job.rb
@@ -2,24 +2,33 @@
 
 ##
 # a job class that will update a Solr document
-# with the extracted text of an attached PDF
+# with the extracted text of  attached PDFs
 class FullTextToSolrJob < ApplicationJob
   # @return [String] the id for the SolrDocument being updated
   # @return [IO] the PDF content
-  attr_reader :solr_document_id, :work_content
+  attr_reader :work_id
 
   queue_as :default
 
-  def initialize(solr_document_id, work_content)
-    @solr_document_id = solr_document_id
-    @work_content = work_content
+  def initialize(work_id)
+    @work_id = work_id
   end
 
   def perform
     solr = RSolr.connect(url: ActiveFedora::SolrService.instance.conn.uri.to_s)
-    solr_document = SolrDocument.find(@solr_document_id).to_h
-    solr_document["all_text_timv"] = FullTextExtractor.new(@work_content).text
+    solr_document = SolrDocument.find(@work_id).to_h
+    solr_document["all_text_timv"] = all_text
     solr.add(solr_document)
     solr.commit
   end
+
+  private
+
+    def files
+      ActiveFedora::Base.find(@work_id).file_sets.map(&:files).flatten
+    end
+
+    def all_text
+      files.map { |file| FullTextExtractor.new(file.content).text }.join("")
+    end
 end
