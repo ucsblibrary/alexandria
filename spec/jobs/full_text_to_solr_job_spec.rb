99c2c37495c508f8bb2de8c034bd52f4898e6482
Jamie Little <jamie@jamielittle.org>
2018-03-08 11:12:12 -0600

    Index text of all attached PDFs on a work
    This commit changes the `FullTextToSolrJob` so that
    it indexes the text content of all PDFs that are attached
    to a work.
    
    This adds another PDF fixture for the updated specs.
    
    This also moves the spec for the `FullTextExtractor` class into
    `spec/services`. The class was moved, but not the spec in an
    earlier commit.
    
    Closes curationexperts/alexandria#52

--- spec/jobs/full_text_to_solr_job_spec.rb
+++ spec/jobs/full_text_to_solr_job_spec.rb
@@ -3,19 +3,50 @@
 require "rails_helper"
 
 RSpec.describe FullTextToSolrJob do
-  let(:file) do
-    File.open(Rails.root.join("spec", "fixtures", "pdf", "sample_full.pdf")).read
+  let(:first_file) do
+    Class.new do
+      def self.content
+        File.open(
+          Rails.root.join("spec", "fixtures", "pdf", "sample_full.pdf")
+        ).read
+      end
+    end
   end
+  let(:second_file) do
+    Class.new do
+      def self.content
+        File.open(
+          Rails.root.join("spec", "fixtures", "pdf", "nexus_200106_artsupp.pdf")
+        ).read
+      end
+    end
+  end
+
+  let(:files_from_fileset) do
+    [first_file, second_file]
+  end
+
   let(:image) { FactoryBot.create(:public_image) }
-  let(:ftts) { described_class.new(image.id, file) }
+  let(:ftts) { described_class.new(image.id) }
   let(:solr) { RSolr.connect(url: ActiveFedora::SolrService.instance.conn.uri.to_s) }
 
-  describe "#perform" do
-    it "adds the text to solr index" do
+  describe "#perform_now" do
+    before do
+      allow(ftts).to receive(:files).and_return(files_from_fileset)
       ftts.perform_now
-      response = solr.get "select", params: { q: "John Arnhold", qf: "all_text_timv" }
+    end
+
+    it "adds the text to solr index from the first pdf" do
+      response = solr.get "select", params:
+                                      { q: "John Arnhold", qf: "all_text_timv" }
       num_found = response["response"]["numFound"]
       expect(num_found).to eq(1)
+    end
+    it "adds the text to the solr index from the second pdf" do
+      response = solr.get "select", params:
+                                      { q: "Kqyaanisqatsi", qf: "all_text_timv" }
+      num_found = response["response"]["numFound"]
+      expect(num_found).to eq(2)
     end
   end
 end
