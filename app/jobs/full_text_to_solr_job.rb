a728aa7abc5de5a3954d24adffaecb830f4cc7b9
Jamie Little <jamie@jamielittle.org>
2018-03-13 09:32:59 -0500

    Full-text indexing on imported objects
    This modifies the object_factory class used
    by the importer to perform a full text indexing
    job on the object.
    
    This is tested by a new spec that that imports a CSV
    with an attached PDF and performs a solr query to check
    for text that is inside the PDF.
    
    Closes curationexperts/alexandria#48

--- app/jobs/full_text_to_solr_job.rb
+++ app/jobs/full_text_to_solr_job.rb
@@ -6,29 +6,33 @@
 class FullTextToSolrJob < ApplicationJob
   # @return [String] the id for the SolrDocument being updated
   # @return [IO] the PDF content
-  attr_reader :work_id
+  attr_reader :work_id, :logger
 
   queue_as :default
 
-  def initialize(work_id)
+  def initialize(work_id, logger = Logger.new(STDOUT))
     @work_id = work_id
+    @logger = logger
   end
 
   def perform
-    solr = RSolr.connect(url: ActiveFedora::SolrService.instance.conn.uri.to_s)
+    return if all_text.nil?
     solr_document = SolrDocument.find(@work_id).to_h
     solr_document["all_text_timv"] = all_text
-    solr.add(solr_document)
-    solr.commit
+    ActiveFedora::SolrService.add(solr_document)
+    ActiveFedora::SolrService.commit
+    @logger.info("Full text indexed for work: #{@work_id}")
   end
 
   private
 
     def files
+      return nil if ActiveFedora::Base.find(@work_id).class == Collection
       ActiveFedora::Base.find(@work_id).file_sets.map(&:files).flatten
     end
 
     def all_text
+      return nil if files.nil?
       files.map { |file| FullTextExtractor.new(file.content).text }.join("")
     end
 end
