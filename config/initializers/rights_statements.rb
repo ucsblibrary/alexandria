5cff0980e17d7807713e2bf78f7ce4796bdb11b0
Bess Sadler <bess@curationexperts.com>
2018-03-21 14:55:02 -0400

    Removing again

--- config/initializers/rights_statements.rb
+++ config/initializers/rights_statements.rb
@@ -4,11 +4,13 @@
 # this puts a valid label into Marmotta so we don't hit
 # Europeana multiple times without effect.
 
-stmt = ControlledVocabularies::RightsStatement.new(
-  "http://www.europeana.eu/rights/unknown/"
-)
-
-stmt << RDF::Statement.new(stmt.rdf_subject,
-                           RDF::RDFS.label, "Unknown copyright status")
-
-stmt.persist!
+# This is preventing us from being able to deploy on AWS. Commenting it out
+# until we're better able to debug.
+# stmt = ControlledVocabularies::RightsStatement.new(
+#   "http://www.europeana.eu/rights/unknown/"
+# )
+# 
+# stmt << RDF::Statement.new(stmt.rdf_subject,
+#                            RDF::RDFS.label, "Unknown copyright status")
+# 
+# stmt.persist!
