b5bc1c945af4127f4af9e19be1b1fe1a6e87f1d5
Suprita Srinivas <suprita.srinivas@ucsb.edu>
2019-09-10 12:33:57 -0700

    Begin comsuming merritt feed through an impirt task. Add ActiveRecord::Base models to capture  paginated feed details and details of each feed entry(ETD).

---
+++ spec/models/merritt/feed_spec.rb
@@ -0,0 +1,51 @@
+require "rails_helper"
+require 'httparty'
+
+describe Merritt::Feed do
+  let(:etd_path) { "/object/recent.atom?collection=ark:/13030/m5pv6m0x" }
+  let(:etd_feed) { Merritt::Feed::HOME + etd_path }
+  let(:page_one) { etd_feed + "&page=1" }
+  let(:feed_file) { File.join(fixture_path, "merritt", "feed.xml") }
+  let(:feed_parsed) { Feedjira.parse File.read(feed_file) }
+
+  before do
+    VCR.turn_off!(:ignore_cassettes => true)
+    stub_request(:get, etd_feed).
+        to_return(headers: {}, body: File.read(feed_file), status: [200, "OK"])
+    HTTParty.get(etd_feed)
+  end
+
+  describe ".etd_feed_url" do
+    it "returns first page of the feed" do
+      expect(described_class.etd_feed_url).to eq(page_one)
+    end
+
+    describe "when page parameter present" do
+      it "returns requested page of the feed" do
+        expect(described_class.etd_feed_url(2)).to eq(etd_feed + "&page=2")
+      end
+    end
+  end
+
+  describe "Feedjira parsed" do
+    before { allow(described_class).to receive(:parse).and_return(feed_parsed) }
+
+    describe ".current_page" do
+      it "returns current page of the feed" do
+        expect(described_class.current_page).to eq(1)
+      end
+    end
+
+    describe ".first_page" do
+      it "returns first page of the feed" do
+        expect(described_class.first_page).to eq(1)
+      end
+    end
+
+    describe ".last_page" do
+      it "returns last page of the feed" do
+        expect(described_class.last_page).to eq(311)
+      end
+    end
+  end
+end
